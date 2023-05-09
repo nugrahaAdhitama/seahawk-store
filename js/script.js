422270b1429dd5fdc1b9a695a1627e7d241df80f
nugrahaAdhitama <78828744+nugrahaAdhitama@users.noreply.github.com>
2023-05-09 08:29:35 +0700

    Add Landing Page
    This commit includes the implementation of the Landing Page with Header, Banner, Contact Us section, and Footer.

---
+++ js/script.js
@@ -0,0 +1,9 @@
+document.querySelector('form').addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    let name = document.getElementById('name').value;
+    let email = document.getElementById('email').value;
+    let message = document.getElementById('message').value;
+
+    alert('Thank you for contacting us, ' + name + '! We will get back to you soon.');
+})
